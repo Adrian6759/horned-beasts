daef80d6e4f938b51682027d35bef64c85ac480e
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2022-11-29 18:15:04 -0800

    Completed lab 02

--- src/components/HornedBeast.js
+++ src/components/HornedBeast.js
@@ -1,12 +1,30 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+import Heart from './heart.png'
 
 class HornedBeast extends React.Component {
+    
+    constructor(props){
+        super(props);
+        this.state = {
+
+            clickCount: 0
+        };
+    }
+    handleClick = () => {
+        this.setState({
+            clickCount: this.state.clickCount + 1
+        });
+    }
     render () {
         return ( <div>
-            <h2>{this.props.title}</h2>
-            <p><img src={this.props.imageUrl} alt='' title=''></img>{this.props.imageUrl}</p>
-            <p>{this.props.description}</p>
-            <p></p>
+            <Card >
+                <Card.Title><h1>{this.props.title}</h1></Card.Title>
+                <Card.Img fluid src={this.props.image}/>
+                <Card.Body>Clicks:<img src={Heart} onClick= {this.handleClick} alt='' heigth='40px' width='40px'/> {this.state.clickCount}
+                <p>{this.props.description}</p></Card.Body>
+            </Card>
+            
         </div>
 
         )
@@ -14,3 +32,4 @@ class HornedBeast extends React.Component {
 } 
 
 export default HornedBeast;
+
