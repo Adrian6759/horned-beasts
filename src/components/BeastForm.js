9850fcfcff797d30ec8c7660e3dbf908d4b80add
adrian.hernandez1993@yahoo.com <adrianhernandez@Adrians-MacBook-Pro-2.local>
2022-12-01 23:40:07 -0800

    minor change in name of form element

--- src/components/BeastForm.js
+++ src/components/BeastForm.js
@@ -13,16 +13,13 @@ class BeastForm extends React.Component {
         return(
         <Form>
                 <Form.Label>Horn Selector</Form.Label>
-                <Form.Control onChange = {this.handleChange} as = 'select'>
+                <Form.Select onChange = {this.handleChange} as = 'select'>
                     <option>Select the Amount of Horns You Want</option>
                     <option value='1'>One Horn</option>
                     <option value="2">Two Horns</option>
                     <option value="3">Three Horns</option>
                     <option value="100">One Hundred Horns</option>
-                </Form.Control>
-            {/* <Button variant="primary" type="submit">
-                Submit
-            </Button> */}
+                </Form.Select>
         </Form>
         )
     }
